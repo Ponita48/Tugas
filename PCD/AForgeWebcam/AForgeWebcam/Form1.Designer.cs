8ce822b32a1b5e868a3e2e19b047928ce3f0a0d4
Dwiki Nurkurniawan <nurkurniawandwiki@gmail.com>
2018-05-15 08:54:29 +0700

    First Commit, PCD Video

---
+++ PCD/AForgeWebcam/AForgeWebcam/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace AForgeWebcam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(481, 290);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(481, 88);
+            this.button1.TabIndex = 1;
+            this.button1.TabStop = false;
+            this.button1.Text = "Load Video";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(499, 308);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(486, 88);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start Camera";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(499, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(486, 290);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 408);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ PCD/AForgeWebcam/AForgeWebcam/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Video.FFMPEG;
+using AForge.Imaging.Filters;
+
+namespace AForgeWebcam
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection webCams;
+        VideoCaptureDevice kamera;
+        Bitmap frame;
+        MJPEGStream stream, streamVideo;
+        VideoFileSource baca;
+
+        bool proses = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            //For any other formats
+            if (kamera != null && kamera.IsRunning)
+            {
+                kamera.Stop();
+            }
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                baca = new VideoFileSource(of.FileName);
+                baca.NewFrame += new NewFrameEventHandler(kamera_ProsesFrame);
+                baca.Start();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (baca != null && baca.IsRunning)
+            {
+                baca.Stop();
+            }
+            webCams = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            kamera = new VideoCaptureDevice(webCams[0].MonikerString);
+            kamera.NewFrame += new NewFrameEventHandler(kamera_ProsesFrame);
+            kamera.Start();
+        }
+
+        void kamera_ProsesFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            frame = (Bitmap)eventArgs.Frame.Clone();
+            Grayscale gs = new Grayscale(0.2125, 0.7154, 0.0721);
+            Threshold th = new Threshold(100);
+            Bitmap img = gs.Apply(frame);
+            th.ApplyInPlace(img);
+            pictureBox1.Image = frame;
+            pictureBox2.Image = img;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                if (kamera != null && kamera.IsRunning)
+                {
+                    kamera.Stop();
+                }
+                Environment.Exit(0);
+            }
+        }
+    }
+}
